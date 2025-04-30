4d68c5f0fcbe3fc8150722ac63da30373d31b9f0
Jose Pujante <jose.pujante@numbytes.com>
2025-04-30 09:48:46 +0000

    tailwind theme updated

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="dracula">
+    <html lang="en" data-theme="winter">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
